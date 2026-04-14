0a17c493a9ca0dda0210b0fb36e5af289b7d0ee5
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-04-14 13:42:13 +0200

    practice#2

---
+++ empty_obj_mod.js
@@ -0,0 +1,6 @@
+let person={};
+//adding fields
+person.name="John";
+person.surname="Smith";
+//displaying the names in the object, but in 1 line for a better readability
+console.log(person.name, person.surname);
