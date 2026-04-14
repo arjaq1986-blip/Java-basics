02ecfcc436c3b3d9a2bd4a34fdbdff4432fc81cc
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-04-14 13:15:06 +0200

    practice#1

--- objects.js
+++ objects.js
@@ -2,4 +2,6 @@ let ticket = {start_station: "Warszawa Zachodnia",
 end_station: "Łódź Kaliska",
 price: 39.99
 };
-console.log(ticket);
+console.log(`ticket from: ${ticket.start_station}`);
+console.log(`ticket to: ${ticket.end_station}`);
+console.log(`ticket price: ${ticket.price}`);
