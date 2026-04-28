75faa8f722be4e702a733100c29b7a6d682c60e4
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-04-29 00:01:01 +0200

    a simple example of my solution to the practice exercise

---
+++ volume.js
@@ -0,0 +1,7 @@
+console.log("This simple program will calculate the volume of a box")
+let width = prompt("Volume of the box, enter width", 0); //allows user to input values
+let height = prompt("Volume of the box, enter height", 0); //allows user to input values
+let lenght = prompt("Volume of the box, enter lenght", 0); //allows user to input values
+//and stores them in variables
+let vol=width*height*lenght //calculation of the volume of the box
+alert ("calculated box volume is " + vol) //displays the result of the calculation to the user
