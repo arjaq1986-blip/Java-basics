35c4e7cadce53ac3bfca327403474ddc4ea2fa4d
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-05-05 15:01:39 +0200

    Just a simple use of the operators

---
+++ exercise1.js
@@ -0,0 +1,6 @@
+let number = prompt("Enter your number");
+if(number > 90 && number < 110) {
+    alert("Bingo!")
+} else {
+    alert("Missed, sorry")
+} // this task is so simple there is no need for extra explanation. It is just a basic usage of the section operators
