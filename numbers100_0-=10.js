a9a03e73cf69d5f43496e855c2f11314e75fe06b
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-05-17 12:11:13 +0200

    Exercise 1
    Simple and quick solution

---
+++ numbers100_0-=10.js
@@ -0,0 +1,6 @@
+let i = 100 // Starting point
+while (i >=0) {// assignment specifies to end at 0, so we will loop until i is greater than or >equal< to 0
+    console.log(i);// This will print the current value of i to the console each time the loop runs
+    alert(i);// This will show the current value of i in an alert box each time the loop runs
+    i -= 10
+}// This way we will get the numbers from 100 to 0, decreasing by 10 each time.
