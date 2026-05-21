accfdc4b9a66f233caf5e0493418571974272d16
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-05-21 19:38:29 +0200

    Change sorting order to descending for numbers array

--- exercise1b.js
+++ exercise1b.js
@@ -1,3 +1,3 @@
 let numbers = [50, 10, 40, 30, 20];
 let sorted = numbers.sort((a, b) => b - a);//all we needed for this exercise is just to change the order of sorting in the previous code.
-console.log(sorted); // [10, 20, 30, 40, 50]
+console.log(sorted); // [50, 40, 30, 20, 10]
