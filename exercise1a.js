11243a07a49d07cc6ac0048bb9d48f8f629f1681
Arkadiusz Krzysztof Jagielski <arjaq1986@gmail.com>
2026-05-21 19:35:43 +0200

    ascending function

---
+++ exercise1a.js
@@ -0,0 +1,3 @@
+let numbers = [50, 10, 40, 30, 20];
+let sorted = numbers.sort((a, b) => a - b);//quickest way to shorten the code. We do not even need to use standard function syntax this way.
+console.log(sorted); // [10, 20, 30, 40, 50]
